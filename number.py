2c7d5b3707e2a15ee8e5d55bb49bb090dc8c5edc
Bhavam Bokare <bhavambokare2008@outlook.com>
2022-05-25 18:31:37 +0530

    Created a Python Project which will take phone number from user and print sim name, country name & timezone

---
+++ number.py
@@ -0,0 +1,14 @@
+import phonenumbers
+from phonenumbers import timezone,geocoder,carrier
+
+number = input("Enter your Phone Number with +(region number): ")
+phone = phonenumbers.parse(number)
+time = timezone.time_zones_for_number(phone)
+cr = carrier.name_for_number(phone,"en")
+reg = geocoder.description_for_number(phone,"en")
+
+print(phone)
+print(time)
+print(cr)
+print(reg)
+
